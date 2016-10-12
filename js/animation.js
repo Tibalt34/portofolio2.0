1d205f47c71605e0a90375cf101c63fffc443074
Tibalt34 <thibaut.figueres@gmail.com>
2016-10-12 16:52:13 +0000

    ajout nouvelle fonction scroll js + modif html

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,6 @@
 /************animation transition : apparition progressive du portofolio**********/
 
 $( document ).ready(function() {
-
    $("body").css("display", "none");
    $("body").fadeIn(3600);
    $("a.transition").click(function(event){
@@ -12,21 +11,24 @@ $( document ).ready(function() {
   window.location = linkLocation;
     }
 
-/************function animation de slider sur la barre de navigation******************/
+/************function animation de slider sur la barre de navigation + scroll****************/
 
 $('a[href^="#"]').click(function(){
 	var the_id = $(this).attr("href");
-
 	$('html, body').animate({
 		scrollTop:$(the_id).offset().top
 	}, 'slow');
 	return false;
   });
-});
 
-/**************animation logo scrolltop***************/
-$('document').ready(function(){
-    $("#divscroll").click(function(){
-        $(this).hide();
-    });
+/**************function pour faire disparaitre le scroll sur la partie accueil************/
+
+  var heighHeader = $("#accueil").height();
+$(document).scroll(function(){
+  if ( $(this).scrollTop() > heighHeader ){
+      $("#divscroll").css("z-index", 10);
+  }else{
+     $("#divscroll").css("z-index", -10);
+  }
+});
 });
